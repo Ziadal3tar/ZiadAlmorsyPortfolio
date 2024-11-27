dc6e39a058f22b2d20f12ff1f0658f293f82ba25
ziadAlattar <ziadalattar22@gmail.com>
2024-11-27 04:06:19 +0200

    first commit

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -53,7 +53,7 @@ export class HeaderComponent {
     }, 3000);
   }
   toPdf(){
-    window.open('https://drive.google.com/file/d/12eOgkdTYYwLxkS5ELRYt5wCiI723x22m/view?usp=drivesdk')
+    window.open('https://drive.google.com/file/d/12fABHHJnEhJkvpsKmTl80KxmfbjSsfZE/view')
   }
   click(tap:any){
   this.changeTap.emit(tap);
